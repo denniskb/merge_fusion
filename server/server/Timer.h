1777b22e569a580d65163e55cfc52a8d26e11fb1
Dennis <dennis@bautembach.de>
2014-02-17 23:23:41 +0100

    Implemented a high precision timer for benchmarking purposes.

---
+++ server/server/Timer.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#define NOMINMAX
+#include <Windows.h>
+
+
+
+namespace kppl {
+
+class Timer
+{
+public:
+	Timer();
+
+	/*
+	Restarts the timer.
+	*/
+	void Reset();
+
+	/*
+	Returns the time in seconds that passed since
+	the object was created.
+	*/
+	double Time();
+
+private:
+	LARGE_INTEGER m_start;
+};
+
+}
